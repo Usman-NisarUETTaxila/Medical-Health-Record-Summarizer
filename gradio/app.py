4a16d84b6536eac0f2cfaeefd97e669e39d50095
Usman Nisar <un41960@gmail.com>
2025-09-21 19:57:59 +0500

    Medical Health Record Summarizer V1.0 done

--- gradio/app.py
+++ gradio/app.py
@@ -2,95 +2,67 @@ import gradio as gr
 import time
 import json
 import random
+from prompt_engineering.prompt_template import Patient_Summary_System
+PSS = Patient_Summary_System()
 
-def fetch_medical_report_api():
-    """
-       API
-
-    """
-    time.sleep(2)  #  delay
-    report = {
-    }
-
-    if report == {}:
-        raise gr.Error("Data not loaded!")
-    return json.dumps(report, indent=2)
-
-def summarize_report(report_json): 
-    try:
-        report = json.loads(report_json)
-        summary = f"""
-    ### 🧑 Patient Info
-    - **ID:** {report['patient_id']}
-    - **Age:** {report['age']}
-    - **Gender:** {report['gender']}
-
-    ### 🩺 Diagnosis
-    - **Condition:** {report['diagnosis']}
-
-    ### ⚠️ Key Symptoms
-    - {', '.join(report['symptoms'])}
-
-    ### 💊 Medications
-    - {', '.join(report['medications'])}
-
-    ### 🧪 Lab Results
-    - **BP:** {report['lab_results']['BP']}
-    - **Cholesterol:** {report['lab_results']['Cholesterol']}
-    """
-    except json.decoder.JSONDecodeError as e:
-        raise gr.Error("Cannot Summarize Empty Data.")
-    return summary
+css = """
+.gradio-container p {
+    font-family: 'Arial', sans-serif;
+}
+"""
 
 
-with gr.Blocks(theme=gr.themes.Soft()) as demo:
-    gr.Markdown(
-        """
-        #           🏥 Health Sync - Medical Report Summarizer
-          
-        ---
-        """
-    )
-
-    with gr.Row():
-        with gr.Column(scale=1):
-            gr.Markdown("")
-            fetch_btn = gr.Button(" Fetch Medical Report", variant="primary")
-            report_box = gr.Code(label="Medical Report", language="json", interactive=False)
-
-        with gr.Column(scale=1):
-            gr.Markdown("")
-            summarize_btn = gr.Button(" Generate Summary", variant="primary")
-            output_box = gr.Markdown()
-
-    # Status
-    status = gr.Label(value="", label="Status")
+if __name__ == "__main__":
 
 
-    def fetch_with_loader():
-        yield "", "⏳ Fetching medical report..."
-        report = fetch_medical_report_api()
-        yield report, "✅ Report fetched successfully!"
+    with gr.Blocks(theme=gr.themes.Soft(),css=css) as demo:
+        gr.Markdown(
+            """
+            #           🏥 Health Sync - Medical Report Summarizer
+            
+            ---
+            """
+        )
+
+        with gr.Row():
+            with gr.Column(scale=1):
+                gr.Markdown("")
+                fetch_btn = gr.Button(" Fetch Medical Report", variant="primary")
+                report_box = gr.Code(label="Medical Report", language="json", interactive=False)
+
+            with gr.Column(scale=1):
+                gr.Markdown("")
+                summarize_btn = gr.Button(" Generate Summary", variant="primary")
+                output_box = gr.Markdown()
+
+        # Status
+        status = gr.Label(value="", label="Status")
 
 
-    fetch_btn.click(
-        fn=fetch_with_loader,
-        inputs=None,
-        outputs=[report_box, status]
-    )
+        def fetch_with_loader():
+            yield "", "⏳ Fetching medical report..."
+            report = json.dumps(PSS.get_patient_data(url='http://localhost:8000/patient-app/api/patients/1/'))
+            yield report, "✅ Report fetched successfully!"
 
 
-    def summarize_with_loader(report_json):
-        yield "", "⏳ Summarizing report with AI..."
-        summary = summarize_report(report_json)
-        yield summary, "✅ Summary ready!"
+        fetch_btn.click(
+            fn=fetch_with_loader,
+            inputs=None,
+            outputs=[report_box, status]
+        )
 
 
-    summarize_btn.click(
-        fn=summarize_with_loader,
-        inputs=report_box,
-        outputs=[output_box, status]
-    )
+        def summarize_with_loader():
+            yield "", "⏳ Summarizing report with AI..."
+            summary = PSS.generate_summary(url='http://localhost:8000/patient-app/api/patients/1/')
+            yield summary, "✅ Summary ready!"
 
 
-demo.launch()
+        summarize_btn.click(
+            fn=summarize_with_loader,
+            inputs=None,
+            outputs=[output_box, status]
+        )
+
+
+    demo.launch()
